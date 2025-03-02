26b7343722e3376d3a10366e85883dfffceeeb92
kyong-dev <kyong.dev@gmail.com>
2025-03-02 10:04:31 +0900

    git cache cleared

--- tests/server/example/models.py
+++
@@ -1,8 +0,0 @@
-from django.contrib.auth.models import AbstractUser
-from django.db import models
-
-
-class User(AbstractUser):
-    content_type = models.ForeignKey(
-        "contenttypes.ContentType", on_delete=models.CASCADE, null=True, blank=True
-    )
